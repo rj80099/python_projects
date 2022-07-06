8445489a0e86e2853fc1b8db0ee599c56a86adaf
rj80099 <rohitjain471@yahoo.com>
2022-07-06 20:05:32 +0530

    Added mail merging project which basically replace all the name in a example.txt file to the invited guest name present in invited.txt file and write new text file in ouput folder which can be send for printing..this project helps in understand the underlying concept file and how to manipulate with the data and populate it as automated task

--- 100DaysCodingChalenge/Intermediate/Day24-FilesDir/FileDir/main.py
+++ 100DaysCodingChalenge/Intermediate/Day24-FilesDir/FileDir/main.py
@@ -13,5 +13,15 @@
 #if no file exist with the named in file paramenter
 # python we go ahead and make a new empty file with the name specified
 
-with open("new_file.txt",mode="a") as file:
-    file.write("\nNew text. ")
+# with open("new_file.txt",mode="a") as file:
+#     file.write("\nNew text. ")
+
+
+#here is the challenge:
+#we want to get the file read from the location which is kept on desktop
+
+#note: we need to keep our code in c director in orer to get access to root dir
+with open("/../../Desktop/my_file.txt") as file:  #relative path
+    print(file.read())
+
+
